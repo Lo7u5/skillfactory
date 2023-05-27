416b862114a1d8b16a0f37c126b379b688340512
Lotus <cyymrak@gmail.com>
2023-05-27 21:57:38 +0500

    emergence data input change

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 
 func main() {
 	var n string
-	fmt.Print("Введите целое число: ")
+	fmt.Print("Введите данные: ")
 	_, err := fmt.Scan(&n)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Вы вели число: %s\n", n)
+	fmt.Printf("Вы вели следущие данные: %s\n", n)
 }
